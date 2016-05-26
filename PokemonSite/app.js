05f4484fe8791f0437154c6cd7a99093bffc6a83
ccoreil14 <ccoreil14@gmail.com>
2016-05-25 18:11:17 -0700

    aaa

--- PokemonSite/app.js
+++ PokemonSite/app.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var orm = require("orm");
 var app = express();
@@ -9,44 +8,59 @@ app.use("/images", express.static(__dirname + '/images'));
 
 
 app.use(orm.express("mysql://ayylmao:berningman123@mysql.hlaingfahim.com/w3pokedex", {
-    
-    define : function(db, models, next) {
-			    models.monster = db.define("monster", {
-				        name: String,
-                        number: String,
-				        type1: String,
-				        type2: String,
-				        baseHp: Number,
-				        baseAtk: Number,
-				        baseDef: Number,
-				        baseSpAtk: Number,
-				        baseSpDef: Number,
-				        baseSpeed: Number,
-				        totalStats: Number,
-				        description: String
 
-				    });
-				    next();
-			}
+    define: function (db, models, next) {
+        models.monster = db.define("monster", {
+            name: String,
+            number: String,
+            type1: String,
+            type2: String,
+            baseHp: Number,
+            baseAtk: Number,
+            baseDef: Number,
+            baseSpAtk: Number,
+            baseSpDef: Number,
+            baseSpeed: Number,
+            totalStats: Number,
+            description: String
+
+        });
+        
+        models.weakness = db.define("weakness", {
+            name: String
+        });
+
+        models.ability = db.define("ability", {
+            name: String,
+            description: String
+        });
+        
+        models.monster.hasMany
+        next();
+    }
+    
+    
+
+
 }));
 
 
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-	req.models.monster.all(function(err, results){
-		res.render('index', {
-        	monArr: results
-    	});
-	});    
+    req.models.monster.all(function (err, results) {
+        res.render('index', {
+            monArr: results
+        });
+    });
 });
 
 app.get('/home', function (req, res) {
-	req.models.monster.all(function(err, results){
-		res.render('home', {
-        	monArr: results
-    	});
-	});    
+    req.models.monster.all(function (err, results) {
+        res.render('home', {
+            monArr: results
+        });
+    });
 });
 
 app.listen(3000);
