1f57289bfd47b49d54850d47877d18393dc8d057
brandon berning <brandon berning>
2016-05-25 19:25:33 -0600

    s

--- PokemonSite/scripts/myscript.js
+++ PokemonSite/scripts/myscript.js
@@ -1,12 +1,12 @@
 document.body.onkeyup = function (e) {
     var answer = document.getElementById('search').value.replace(/\s/g, '');;
     var name = $('.monster .name').html();
-    var number = $('.monster .number').html();
-    console.log(number);
+    var type = $('.monster .type').html();
     console.log(answer);
+    console.log(type);
     if (answer === name.replace(/\s/g, '')) {
         console.log('yay');
-    } else if (answer === id.replace(/\s/g, '')) {
+    } else if (answer === type.replace(/\s/g, '')) {
         console.log('yay');
     }
 }
