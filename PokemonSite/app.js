ca7deae352a259cdeb8521cf3a5779128589dced
ccoreil14 <ccoreil14@gmail.com>
2016-06-01 15:09:06 -0700

    made chanages

--- PokemonSite/app.js
+++ PokemonSite/app.js
@@ -35,20 +35,11 @@ app.use(orm.express("mysql://ayylmao:berningman123@mysql.hlaingfahim.com/w3poked
             description: String
         });
 
-//        models.monster.hasMany('weakness', models.weakness, {},{autoFetch:true});
-//        models.monster.hasMany('ability', models.ability, {},{autoFetch:true});
-//
-//        models.monster.get(1, function (err, monster) {
-//            if (err) throw err;
-//            //console.log(monster.weakness)
-//            
-//        });
-//        models.monster.get(1, function (err, monster) {
-//            if (err) throw err;
-//            //console.log(monster.ability);
-//
-//        });
+        models.monster.hasMany('weakness', models.weakness, {},{autoFetch:true});
+        models.monster.hasMany('ability', models.ability, {},{autoFetch:true});
 
+        
+con
         next();
     }
 }));
@@ -61,6 +52,15 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     req.models.monster.all(function (err, results) {
         res.render('home', {
+            monArr: results
+        });
+    });
+});
+
+
+app.get('/:id', function (req, res) {
+    req.models.monster.get(req.params.id,function (err, results) {
+        res.render('singleMon', {
             monArr: results
         });
     });
