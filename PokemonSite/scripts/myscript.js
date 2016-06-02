9debdde0e175a7bd984c80301c74bb39d2b3d81d
ccoreil14 <ccoreil14@gmail.com>
2016-06-02 10:31:47 -0700

    search

--- PokemonSite/scripts/myscript.js
+++ PokemonSite/scripts/myscript.js
@@ -1,12 +1,31 @@
 document.body.onkeyup = function (e) {
-    var answer = document.getElementById('search').value.replace(/\s/g, '');;
-    var name = $('.monster .name').html();
-    var number = $('.monster .number').html();
-    console.log(number);
+    var answer = document.getElementById('search').value.replace(/\s/g, '');
+
+    var name = document.getElementsByClassName('monName');
+    
+
+
     console.log(answer);
-    if (answer === name.replace(/\s/g, '')) {
-        console.log('yay');
-    } else if (answer === id.replace(/\s/g, '')) {
-        console.log('yay');
+
+    for (i = 0; i < name.length; i++) {
+        if (answer === name[i].innerHTML) {
+            for (j = 0; j < name.length; j++) {
+                document.getElementById("poke" + [j+1]).style.display = "none";
+            }
+            document.getElementById("poke" +[i+1]).style.display = "table-row";
+            break;
+        }else{
+            for (j = 0; j < name.length; j++) {
+                document.getElementById("poke" + [j+1]).style.display = "table-row";
+            }
+        }
     }
+
+    //    var number = $('.monster .number').html();
+    //    console.log(number);
+    //    console.log(answer);
+    //    if (answer === name.replace(/\s/g, '')) {
+    //        console.log('yay');
+    //    } 
+
 }
